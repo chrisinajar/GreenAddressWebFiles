bbfddc5a72a41fd952f16b67ab373ef854d4144a
Chris Vickery <chrisinajar@gmail.com>
2016-04-07 11:16:35 -0700

    Completely refactored all of the library usages. App works, but complains about typeahead-popup when in send money

--- static/js/greenwallet/services.js
+++ static/js/greenwallet/services.js
@@ -3748,6 +3748,7 @@ angular.module('greenWalletServices', [])
                         });
                     }
                     app_promise.then(function(btchip) {
+                        if (!btchip) { return; };
                         btchip.app.getFirmwareVersion_async().then(function(version) {
                             if (noModal) {
                                 $interval.cancel(tick);
