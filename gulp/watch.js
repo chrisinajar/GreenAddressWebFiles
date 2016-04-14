511ebd67899df21ffaa91c89c0c5dd4c62941f9c
Chris Vickery <chrisinajar@gmail.com>
2016-04-14 16:50:55 -0700

    Error checking on the build

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
 
 gulp.task('listen', function () {
-  gulp.watch('static/css/**/*.css', ['css']);
-  gulp.watch('static/js/**/*.js', ['js']);
-  gulp.watch('templates/**/*.html', ['templates']);
+  ignoreENOENT(gulp.watch('static/css/**/*.css', ['css']));
+  ignoreENOENT(gulp.watch('static/js/**/*.js', ['js']));
+  ignoreENOENT(gulp.watch('templates/**/*.html', ['templates']));
 });
+
+function ignoreENOENT (gw) {
+  gw.on('error', function(error) {
+    // silently catch 'ENOENT' error typically caused by renaming watched folders
+    if (error.code === 'ENOENT') {
+      return;
+    }
+  })
+}
