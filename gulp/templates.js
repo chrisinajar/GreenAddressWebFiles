9a7bc3a787bcb4aeb606b7b5ba342af7e0afb40a
Chris Vickery <chrisinajar@gmail.com>
2016-04-18 17:05:15 -0700

    Compile cordova static assets as well

--- gulp/templates.js
+++ gulp/templates.js
@@ -3,10 +3,16 @@ var run = require('gulp-run');
 var clean = require('gulp-clean');
 
 gulp.task('clean-templates', function () {
-  return gulp.src('dist/templates', {read: false})
+  return gulp.src([
+      'dist/templates',
+      'dist/cordova-templates'
+    ], {read: false})
     .pipe(clean());
 });
 
-gulp.task('templates', ['clean-templates'], function () {
+gulp.task('templates', ['clean-templates', 'cordova-templates'], function () {
   return run('python render_templates.py dist/templates').exec();
+});
+gulp.task('cordova-templates', ['clean-templates'], function () {
+  return run('python render_templates.py -a dist/cordova-templates').exec();
 });
